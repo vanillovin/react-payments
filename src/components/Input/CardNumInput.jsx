db76e929831c005f29215c5d9e08c1357afd236d
vanillovin <vanillovin@gmail.com>
2022-01-24 11:35:26 +0900

    prettier 적용

--- src/components/Input/CardNumInput.jsx
+++ src/components/Input/CardNumInput.jsx
@@ -5,24 +5,32 @@ import React from 'react';
 
 // 어떨 때 추론? 생각해보기 (깨질수있는것-className, name. 바뀌는가?) -> 안전하고 유연
 // 명확하게 마지막이라는 것을. default 값을 많이 넣기. 추론하게 하지 않고 명시적으로!
-const CardNumInput = ({ type="text", name, value, onChange, final=false }) => {
+const CardNumInput = ({
+  type = 'text',
+  name,
+  value,
+  onChange,
+  final = false,
+}) => {
   return (
-   <>
-    <input 
-      id='card-num'
-      type={type}
-      name={name}
-      value={value}
-      minLength={4} 
-      maxLength={4} 
-      required={true} 
-      className='input-basic' 
-      disabled={false}
-      onChange={onChange}
-    />
-    {!final && <span style={{color: 'black'}}>{value.length >= 4 && '-'}</span>}
-   </>
+    <>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        value={value}
+        minLength={4}
+        maxLength={4}
+        required={true}
+        className="input-basic"
+        disabled={false}
+        onChange={onChange}
+      />
+      {!final && (
+        <span style={{ color: 'black' }}>{value.length >= 4 && '-'}</span>
+      )}
+    </>
   );
 };
 
-export default CardNumInput
+export default CardNumInput;
